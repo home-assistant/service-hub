a1a6c84da69613d7e439aa2a3b88a543b5bfd6f8
Joakim Sørensen <ludeeus@ludeeus.dev>
2022-09-12 22:41:12 +0200

    Better versions command (#6)

--- services/bots/src/discord/discord.module.ts
+++ services/bots/src/discord/discord.module.ts
@@ -2,10 +2,10 @@ import { Module } from '@nestjs/common';
 import { DiscordModule } from '@discord-nestjs/core';
 import { PingCommand } from './commands/ping';
 import { PongCommand } from './commands/pong';
-import { VersionCommand } from './commands/version';
+import { VersionsCommand } from './commands/versions';
 
 @Module({
   imports: [DiscordModule.forFeature()],
-  providers: [PingCommand, PongCommand, VersionCommand],
+  providers: [PingCommand, PongCommand, VersionsCommand],
 })
 export class DiscordBotModule {}
