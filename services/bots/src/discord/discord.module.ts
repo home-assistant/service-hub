bca67d7b9cb9ab71ce7d2cf709276aa6077ba39a
ludeeus <ludeeus@ludeeus.dev>
2022-09-13 06:40:23 +0000

    Remove pong

--- services/bots/src/discord/discord.module.ts
+++ services/bots/src/discord/discord.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { DiscordModule } from '@discord-nestjs/core';
 import { PingCommand } from './commands/ping';
-import { PongCommand } from './commands/pong';
 import { VersionsCommand } from './commands/versions';
 
 @Module({
   imports: [DiscordModule.forFeature()],
-  providers: [PingCommand, PongCommand, VersionsCommand],
+  providers: [PingCommand, VersionsCommand],
 })
 export class DiscordBotModule {}
