8e5b1fac51054845cbdfb1c2765e2b82a437bfc8
ludeeus <ludeeus@ludeeus.dev>
2022-09-21 11:23:34 +0000

    Add test for context

--- tests/utils/test_context.ts
+++ tests/utils/test_context.ts
@@ -5,12 +5,12 @@ export class MockWebhookContext extends WebhookContext<any> {}
 export const mockWebhookContext = (params?: Partial<WebhookContext<any>>): WebhookContext<any> =>
   new WebhookContext({
     // @ts-ignore
-    github: { ...params.github },
+    github: { ...params?.github },
     payload: {
       repository: { name: 'core', owner: { login: 'home-assistant' } },
       sender: { type: 'user', login: 'test-developer#1337' },
       number: 1337,
       ...params?.payload,
     },
-    eventType: params.eventType || '',
+    eventType: params?.eventType || '',
   });
