b8ab32f832e433c0181e172af09ef6f40306618f
ludeeus <ludeeus@ludeeus.dev>
2022-09-22 10:10:19 +0000

    Use pullRequest instead of issue

--- services/bots/src/github-webhook/utils/pull_request.ts
+++ services/bots/src/github-webhook/utils/pull_request.ts
@@ -5,7 +5,7 @@ export const fetchPullRequestFilesFromContext = async (
   context: WebhookContext<any>,
 ): Promise<ListPullRequestFiles> => {
   if (!context._prFilesCache) {
-    context._prFilesCache = (await context.github.pulls.listFiles(context.issue())).data;
+    context._prFilesCache = (await context.github.pulls.listFiles(context.pullRequest())).data;
   }
   return context._prFilesCache;
 };
