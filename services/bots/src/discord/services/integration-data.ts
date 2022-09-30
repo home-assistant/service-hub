f0d4b3ffc5704b52851dc20da655f2e2fdd65634
Joakim Sørensen <ludeeus@ludeeus.dev>
2022-09-30 20:16:06 +0200

    Use services for data operations in discord bot (#59)

---
+++ services/bots/src/discord/services/integration-data.ts
@@ -0,0 +1,24 @@
+import fetch from 'node-fetch';
+import { Injectable } from '@nestjs/common';
+
+export interface IntegrationData {
+  title: string;
+  quality_scale: string;
+  iot_class: string;
+}
+
+@Injectable()
+export class IntegrationDataService {
+  public data: { [key: string]: IntegrationData };
+
+  public async getIntegration(domain: string): Promise<IntegrationData | undefined> {
+    await this.ensureData();
+    return this.data?.[domain];
+  }
+
+  public async ensureData(force?: boolean) {
+    if (force || !this.data) {
+      this.data = await (await fetch('https://www.home-assistant.io/integrations.json')).json();
+    }
+  }
+}
