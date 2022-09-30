d81d3377e7154a1dec7bb910991851336f27631a
Joakim Sørensen <ludeeus@ludeeus.dev>
2022-09-30 15:43:46 +0200

    Add modal to handle params for my links (#58)

--- services/bots/src/discord/commands/my.ts
+++ services/bots/src/discord/commands/my.ts
@@ -3,15 +3,24 @@ import fetch from 'node-fetch';
 import { TransformPipe } from '@discord-nestjs/common';
 import {
   DiscordTransformedCommand,
+  On,
+  Param,
   Payload,
   TransformedCommandExecutionContext,
-  Param,
   UsePipes,
-  On,
 } from '@discord-nestjs/core';
-import { CommandHandler, DiscordCommandClass } from '../discord.decorator';
-import { AutocompleteInteraction, EmbedBuilder } from 'discord.js';
 import { reportException } from '@lib/sentry/reporting';
+import {
+  ActionRowBuilder,
+  AutocompleteInteraction,
+  EmbedBuilder,
+  ModalActionRowComponentBuilder,
+  ModalBuilder,
+  ModalSubmitInteraction,
+  TextInputBuilder,
+  TextInputStyle,
+} from 'discord.js';
+import { CommandHandler, DiscordCommandClass } from '../discord.decorator';
 
 interface Redirect {
   redirect: string;
@@ -22,7 +31,7 @@ interface Redirect {
   description: string;
   introduced?: string;
   component?: string;
-  params?: any;
+  params?: Record<string, string>;
 }
 
 class MyDto {
@@ -81,60 +90,93 @@ export class MyCommand implements DiscordTransformedCommand<MyDto> {
       return;
     }
 
+    if (redirectData.params) {
+      await interaction.showModal(
+        new ModalBuilder({
+          title: 'Additional data',
+          customId: redirectData.redirect,
+          components: Object.entries(redirectData.params).map(([key, keyType]) =>
+            new ActionRowBuilder<ModalActionRowComponentBuilder>().addComponents(
+              new TextInputBuilder({
+                custom_id: key,
+                label: key,
+                required: !keyType.includes('?'),
+                style: TextInputStyle.Short,
+              }),
+            ),
+          ),
+        }),
+      );
+      return;
+    }
+
     await interaction.reply({
       embeds: [
         new EmbedBuilder({
           title: redirectData.name,
           description: redirectData.description,
-          url: redirectData.params
-            ? `https://my.home-assistant.io/create-link/?redirect=${redirectData.redirect}`
-            : `https://my.home-assistant.io/redirect/${redirectData.redirect}/`,
-          footer: redirectData.params
-            ? { text: 'This My link needs adjustments before usage' }
-            : undefined,
+          url: `https://my.home-assistant.io/create-link/?redirect=${redirectData.redirect}`,
         }),
       ],
     });
   }
 
-  // This is the autocomplete handler for the /my command
   @On('interactionCreate')
-  async onInteractionCreate(interaction: AutocompleteInteraction): Promise<void> {
-    if (!interaction.isAutocomplete() || interaction.commandName !== 'my') {
-      return;
-    }
-    try {
-      await this.ensureRedirects();
-      const focusedValue = interaction.options.getFocused()?.toLowerCase();
+  async onInteractionCreate(
+    interaction: AutocompleteInteraction | ModalSubmitInteraction,
+  ): Promise<void> {
+    // This is the autocomplete handler for the /my command
+    if (interaction.isAutocomplete() && interaction.commandName === 'my') {
+      try {
+        await this.ensureRedirects();
+        const focusedValue = interaction.options.getFocused()?.toLowerCase();
 
-      await interaction.respond(
-        focusedValue.length !== 0
-          ? this.redirects
-              .filter((redirect) => !redirect.deprecated)
-              .map((redirect) => ({
-                name: redirect.name,
-                value: redirect.redirect,
-              }))
-              .filter(
-                (choice) =>
-                  choice.value.toLowerCase().includes(focusedValue) ||
-                  choice.name.toLowerCase().includes(focusedValue),
-              )
-              // The API only allow max 25 sugestions
-              .slice(0, 25)
-          : [],
-      );
-    } catch (err) {
-      reportException(err, {
-        cause: err,
-        data: {
-          interaction: interaction.toJSON(),
-          user: interaction.user.toJSON(),
-          channel: interaction.channel.toJSON(),
-          command: interaction.command.toJSON(),
-        },
+        await interaction.respond(
+          focusedValue.length !== 0
+            ? this.redirects
+                .filter((redirect) => !redirect.deprecated)
+                .map((redirect) => ({
+                  name: redirect.name,
+                  value: redirect.redirect,
+                }))
+                .filter(
+                  (choice) =>
+                    choice.value.toLowerCase().includes(focusedValue) ||
+                    choice.name.toLowerCase().includes(focusedValue),
+                )
+                // The API only allow max 25 sugestions
+                .slice(0, 25)
+            : [],
+        );
+      } catch (err) {
+        reportException(err, {
+          cause: err,
+          data: {
+            interaction: interaction.toJSON(),
+            user: interaction.user.toJSON(),
+            channel: interaction.channel.toJSON(),
+            command: interaction.command.toJSON(),
+          },
+        });
+        await interaction.respond([]);
+      }
+    } // This is modal submition handler if the redirect supports params
+    else if (interaction.isModalSubmit()) {
+      const redirectData = this.redirects.find((entry) => entry.redirect === interaction.customId);
+      console.log(JSON.stringify(interaction.fields.fields));
+      const url = new URL(`https://my.home-assistant.io/redirect/${redirectData.redirect}/`);
+      for (const field of interaction.fields.fields.values()) {
+        url.searchParams.set(field.customId, field.value);
+      }
+      await interaction.reply({
+        embeds: [
+          new EmbedBuilder({
+            title: redirectData.name,
+            description: redirectData.description,
+            url: url.toString(),
+          }),
+        ],
       });
-      await interaction.respond([]);
     }
   }
 }
