79814aed39c1a50c82764d960c5885f45474e4ad
Joakim Sørensen <ludeeus@ludeeus.dev>
2022-09-30 10:32:33 +0200

    Add MyCommand to discord bot (#56)

--- services/bots/src/discord/discord.module.ts
+++ services/bots/src/discord/discord.module.ts
@@ -4,9 +4,10 @@ import { PingCommand } from './commands/ping';
 import { VersionsCommand } from './commands/versions';
 import { IntegrationCommand } from './commands/integration';
 import { MessageCommand } from './commands/message';
+import { MyCommand } from './commands/my';
 
 @Module({
   imports: [DiscordModule.forFeature()],
-  providers: [PingCommand, VersionsCommand, IntegrationCommand, MessageCommand],
+  providers: [PingCommand, VersionsCommand, IntegrationCommand, MessageCommand, MyCommand],
 })
 export class DiscordBotModule {}
