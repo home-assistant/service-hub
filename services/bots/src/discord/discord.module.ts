dc92d86a41b50e56f95e68dbb5bbb06a0f633640
ludeeus <ludeeus@ludeeus.dev>
2022-10-04 17:05:52 +0000

    Add multi guild support to discord bot

--- services/bots/src/discord/discord.module.ts
+++ services/bots/src/discord/discord.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { DynamicModule, Inject, Module, ModuleMetadata } from '@nestjs/common';
 import { DiscordModule } from '@discord-nestjs/core';
 import { PingCommand } from './commands/ping';
 import { VersionsCommand } from './commands/versions';
@@ -7,17 +7,55 @@ import { MessageCommand } from './commands/message';
 import { MyCommand } from './commands/my';
 import { IntegrationDataService } from './services/integration-data';
 import { MyRedirectDataService } from './services/my-redirect-data';
+import { DiscordGuild } from './discord.const';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { GatewayIntentBits } from 'discord.js';
+import Config from '../config';
 
-@Module({
-  imports: [DiscordModule.forFeature()],
-  providers: [
+const config = Config.getProperties();
+
+const PROVIDERS = {
+  global: [PingCommand],
+  [DiscordGuild.HOME_ASSISTANT]: [
     IntegrationDataService,
     MyRedirectDataService,
-    PingCommand,
     VersionsCommand,
     IntegrationCommand,
     MessageCommand,
     MyCommand,
   ],
+};
+
+@Module({
+  imports: [
+    DiscordModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        token: configService.get('discord.token'),
+        discordClientOptions: {
+          intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
+        },
+        registerCommandOptions: [
+          {
+            forGuild: configService.get('discord.guildId'),
+            removeCommandsBefore: true,
+          },
+        ],
+        failOnLogin: true,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  providers: [],
 })
-export class DiscordBotModule {}
+export class DiscordBotModule {
+  static register(): DynamicModule {
+    return {
+      module: DiscordBotModule,
+      providers: [
+        ...PROVIDERS.global,
+        ...(config.discord.guildId in PROVIDERS ? PROVIDERS[config.discord.guildId] : []),
+      ],
+    };
+  }
+}
