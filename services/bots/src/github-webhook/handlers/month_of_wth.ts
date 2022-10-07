bbf73eb8bcd3635f127ea2d6c7dbc9e01e211bd2
ludeeus <ludeeus@ludeeus.dev>
2022-10-07 04:43:45 +0000

    Fallback for missing body

--- services/bots/src/github-webhook/handlers/month_of_wth.ts
+++ services/bots/src/github-webhook/handlers/month_of_wth.ts
@@ -13,7 +13,9 @@ export class MonthOfWTH extends BaseWebhookHandler {
   public allowedOrganizations = [Organization.HOME_ASSISTANT];
 
   async handle(context: WebhookContext<PullRequestOpenedEvent>) {
-    for (const link of extractForumLinks((context.payload.pull_request as PullRequest).body)) {
+    for (const link of extractForumLinks(
+      (context.payload.pull_request as PullRequest).body || '',
+    )) {
       try {
         const linkData = await (await fetch(`${link}.json`)).json();
         if (linkData.category_id === WTH_CATEGORY_ID) {
