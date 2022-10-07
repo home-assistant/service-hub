cd06c1dc70be5cb9e16d8b4c8096147ed526950b
Joakim Sørensen <ludeeus@ludeeus.dev>
2022-10-07 15:12:24 +0200

    Add global TopicCommand (#92)

--- services/bots/src/discord/discord.const.ts
+++ services/bots/src/discord/discord.const.ts
@@ -1,4 +1,15 @@
+import { Param } from '@discord-nestjs/core';
+
 export class BlankDto {}
+
+export class OptionalUserMentionDto {
+  @Param({
+    name: 'user',
+    description: 'Tag the user you want the message to be posted for',
+    required: false,
+  })
+  userMention: string;
+}
 
 export enum Emoji {
   FIRST_PLACE = ':first_place:',
