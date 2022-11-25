47f0d29ce8121ad0a1adc5bf74a7ee02ce5f8141
ludeeus <ludeeus@ludeeus.dev>
2022-11-25 07:46:26 +0000

    Limit quality scale

--- services/bots/src/github-webhook/handlers/quality_scale.ts
+++ services/bots/src/github-webhook/handlers/quality_scale.ts
@@ -14,7 +14,35 @@ export class QualityScaleLabeler extends BaseWebhookHandler {
       return;
     }
 
-    const domain = context.payload.label.name.split('integration: ')[1];
+    // When a PR is opened there can be a lot of labels added.
+    // We only want to add quality scale labels if only 1 integration is touched.
+    // To handle this we need to do an API call to get the current labels.
+    const currentLabels = (await context.github.issues.listLabelsOnIssue(context.issue())).data.map(
+      (label) => label.name,
+    );
+
+    const currentIntegrationLabels = currentLabels.filter((label) =>
+      label.startsWith('integration: '),
+    );
+
+    if (currentIntegrationLabels.length !== 1) {
+      // Check if we need to cleanup labels
+      const qualityScaleLabels = currentLabels.filter((label) =>
+        label.startsWith('Quality Scale: '),
+      );
+      if (qualityScaleLabels.length !== 0) {
+        for (const label of qualityScaleLabels) {
+          try {
+            await context.github.issues.removeLabel(context.issue({ name: label }));
+          } catch (_) {
+            // Ignore issues here.
+          }
+        }
+      }
+      return;
+    }
+
+    const domain = currentIntegrationLabels[0].split('integration: ')[1];
     const manifest = await fetchIntegrationManifest(domain);
 
     if (manifest) {
