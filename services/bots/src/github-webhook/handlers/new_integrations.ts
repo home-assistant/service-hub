4cfeaef2157dc6df271fcd3297ccd170d0c12022
ludeeus <joasoe@gmail.com>
2023-02-22 10:36:42 +0000

    Simplify handling

--- services/bots/src/github-webhook/handlers/new_integrations.ts
+++ services/bots/src/github-webhook/handlers/new_integrations.ts
@@ -6,19 +6,13 @@ import { BaseWebhookHandler } from './base';
 import { fetchPullRequestFilesFromContext } from '../utils/pull_request';
 import { ParsedPath } from '../utils/parse_path';
 
-const REVIEW_COMMENT = `
-When adding new integrations. Limit included platforms to a single platform, use the **Ready for review** button when you are done, thanks :+1:
-
-[_Learn more about our pull request process._](https://developers.home-assistant.io/docs/review-process/#home-assistant-core)
-`;
-
 export class NewIntegrationsHandler extends BaseWebhookHandler {
   public allowedEventTypes = [EventType.PULL_REQUEST_LABELED];
   public allowedRepositories = [HomeAssistantRepository.CORE];
 
   /**
    * When a new-integration label is added, check if the PR contains multiple platforms.
-   * If so, convert the PR to a draft and request changes.
+   * If so, request changes. The ReviewDrafter will handle the rest.
    */
   async handle(context: WebhookContext<PullRequestLabeledEvent>) {
     if (context.payload.label?.name !== 'new-integration') {
@@ -33,11 +27,10 @@ export class NewIntegrationsHandler extends BaseWebhookHandler {
     if (integrationPlatforms.length > 1) {
       await context.github.pulls.createReview(
         context.pullRequest({
-          body: REVIEW_COMMENT,
+          body: '[When adding new integrations. Limit included platforms to a single platform](https://developers.home-assistant.io/docs/review-process/#home-assistant-core)',
           event: 'REQUEST_CHANGES',
         }),
       );
-      await context.convertPullRequestToDraft(context.payload.pull_request.node_id);
     }
   }
 }
