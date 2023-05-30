8a2c12bc08e10a5f4d2e0f2592d039fb979c643d
ludeeus <joasoe@gmail.com>
2023-05-30 19:27:59 +0000

    check for login

--- services/bots/src/github-webhook/handlers/validate-cla.ts
+++ services/bots/src/github-webhook/handlers/validate-cla.ts
@@ -88,7 +88,7 @@ export class ValidateCla extends BaseWebhookHandler {
         continue;
       }
 
-      if (!commit.author || Object.keys(commit.author).length === 0) {
+      if (!commit.author?.login) {
         commitsWithoutLogins.push({
           sha: commit.sha,
           maybeText: commit.commit?.author?.email?.includes('@')
