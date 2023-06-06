50f276d45219b77aef7eafa5df212c99994fe9a2
Raman Gupta <7243222+raman325@users.noreply.github.com>
2023-06-06 09:38:32 -0400

    don't modify original list

--- services/bots/src/github-webhook/utils/organization_teams.ts
+++ services/bots/src/github-webhook/utils/organization_teams.ts
@@ -7,20 +7,20 @@ export const expandOrganizationTeams = async (
   usersAndTeams: string[],
 ): Promise<string[]> => {
   // Remove the `@` and lowercase
-  usersAndTeams = usersAndTeams.map((name) =>
+  const usersAndTeamsCopy = usersAndTeams.map((name) =>
     name.startsWith('@') ? name.substring(1).toLowerCase() : name.toLowerCase(),
   );
   // Get teams from list
-  const teamNames = usersAndTeams.filter((name) =>
+  const teamNames = usersAndTeamsCopy.filter((name) =>
     name.startsWith(`${context.organization}${ORG_TEAM_SEP}`),
   );
-  // For each team in usersAndTeams, add the members of the team to the list
+  // For each team in usersAndTeamsCopy, add the members of the team to the list
   for (const teamName in teamNames) {
     const teamMembers = await context.github.teams.listMembersInOrg({
       org: context.organization,
       team_slug: teamName.split(ORG_TEAM_SEP)[1],
     });
-    usersAndTeams.push(...teamMembers.data.map((member) => member.login.toLowerCase()));
+    usersAndTeamsCopy.push(...teamMembers.data.map((member) => member.login.toLowerCase()));
   }
-  return usersAndTeams;
+  return usersAndTeamsCopy;
 };
