9aa71b99d05124b6ac1a22d5178eec8500841288
ludeeus <joasoe@gmail.com>
2023-12-20 10:27:49 +0000

    Enable MergeConflictChecker plugin for esphome/esphome

--- services/bots/src/github-webhook/handlers/merge_conflict.ts
+++ services/bots/src/github-webhook/handlers/merge_conflict.ts
@@ -1,11 +1,11 @@
 import { PullRequestOpenedEvent, PullRequestSynchronizeEvent } from '@octokit/webhooks-types';
-import { EventType, HomeAssistantRepository } from '../github-webhook.const';
+import { ESPHomeRepository, EventType, HomeAssistantRepository } from '../github-webhook.const';
 import { WebhookContext } from '../github-webhook.model';
 import { BaseWebhookHandler } from './base';
 
 export class MergeConflictChecker extends BaseWebhookHandler {
   public allowedEventTypes = [EventType.PULL_REQUEST_OPENED, EventType.PULL_REQUEST_SYNCHRONIZE];
-  public allowedRepositories = [HomeAssistantRepository.CORE];
+  public allowedRepositories = [HomeAssistantRepository.CORE, ESPHomeRepository.ESPHOME];
 
   async handle(context: WebhookContext<PullRequestOpenedEvent | PullRequestSynchronizeEvent>) {
     // The data in the event is stale, so we need to re-fetch it.
